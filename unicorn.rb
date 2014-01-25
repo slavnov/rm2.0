43653c369176a96fa9cd07b408add2c5c08513ca
Alexey Vildyaev <hav0k@me.com>
2014-01-26 03:57:15 +0400

    fix quotes

--- unicorn.rb
+++ unicorn.rb
@@ -1,8 +1,8 @@
 worker_processes 8
-working_directory “/web/rm2.0/current”
-listen ‘unix:/tmp/basic.sock’, :backlog => 512
+working_directory '/web/rm2.0/current'
+listen 'unix:/tmp/basic.sock', :backlog => 512
 timeout 120
-pid “/var/run/unicorn/basic_unicorn.pid”
+pid '/var/run/unicorn/basic_unicorn.pid'
 
 preload_app true
 if GC.respond_to?(:copy_on_write_friendly=)
@@ -10,10 +10,10 @@ if GC.respond_to?(:copy_on_write_friendly=)
 end
 
 before_fork do |server, worker|
-  old_pid = “#{server.config[:pid]}.oldbin”
+  old_pid = '#{server.config[:pid]}.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
-      Process.kill(“QUIT”, File.read(old_pid).to_i)
+      Process.kill('QUIT', File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
       # someone else did our job for us
     end
